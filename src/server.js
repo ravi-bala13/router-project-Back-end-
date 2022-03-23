fedb29ad5776841eaf4a92ea1de92b3ef005b8c6
Bala Murugan R <bals161616@gmail.com>
2022-03-23 17:10:34 +0530

    port number make as dynamic

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require("./index");
 const connect = require("./configs/db");
 
-app.listen(1234, async () => {
+require("dotenv").config();
+
+const port = process.env.PORT || 1234;
+
+app.listen(port, async () => {
   await connect();
   console.log("Hai dear friend i am listening 1234");
 });
